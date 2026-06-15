9ba4a7fb777a215f591b9d0ca80a45361602c7f4
KimPickart <pickart.kim@gmail.com>
2026-06-15 17:15:49 +0200

    uploading php project

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notizen</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="symbol.png" alt="" class="symbol_img">
+        <p class="header-to-do">To-Do Liste</p>
+    </header>
+    
+    <div class="content-area">
+        <h1>Notizen</h1>
+        
+        <div class="enter_area">
+            <input placeholder="To-Do" type="text" class="input-field">
+            <button class="enter_button">Einfügen</button>
+        </div>
+        
+        <div class="To-Do">
+            <div class="note">
+                <img src="note_without.png" alt="" class="img_without">
+                <p class="in_progress_to_do">Hallo</p>   
+                <img src="delead.png" alt="" class="delead">
+            </div>   
+            <div class="note">
+                <img src="note_with.png" alt="" class="img_with">
+                <p class="finish_to_do">Hallo</p>   
+                <img src="delead.png" alt="" class="delead">
+            </div>          
+        </div>
+    </div>
+
+    <footer></footer>
+</body>
+</html>
